4e580bcef46e82cd69575b8a453d661ed07b0bf1
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2023-02-15 13:00:16 +0100

    result

--- Timer.cpp
+++
@@ -1,8 +0,0 @@
-#include "Timer.hpp"
-void Timer::wait(unsigned long long t) {
-	//insert function to block the execution
-}
-unsigned long long Timer::milliseconds() {
-	//insert function to return milliseconds
-	return 0;
-}
