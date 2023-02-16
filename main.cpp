832c4eb614e346dd177ae30254def713eb9ef580
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2023-02-16 20:48:58 +0100

    input file and output dir check to prevent runtime errors

--- main.cpp
+++ main.cpp
@@ -6,13 +6,41 @@
 #include <QXmlStreamReader>
 
 int main(int argc, char* argv[]) {
-	QString xmlFileName = QString(argv[1]);
 
+	QString xmlFileName;
+	if(argc > 1) {
+		xmlFileName = QString(argv[1]);
+		if(xmlFileName.last(4) != ".xml") {
+			qWarning() << "input file is not a .xml file";
+			return -1;
+		}
+	}
+	else {
+		qWarning() << "no input file given";
+		return -1;
+	}
+
+	QString outDirName;
+	if(argc > 2) {
+		outDirName = QString(argv[2]);
+		if(outDirName.contains("\\") || outDirName.contains("/")) {
+			qWarning() << "sub-folders aren't currently supported";
+			return -1;
+		}
+	}
+	else outDirName = QString("out");
+
+
+	QDir outDir = QDir(outDirName);
 	QFile xmlFile = QFile(xmlFileName);
+	if(!xmlFile.exists()) {
+		qWarning() << xmlFileName << "does not exists";
+		return -1;
+	}
+
 	xmlFile.open(QIODevice::ReadOnly | QIODevice::Text);
 	auto* xml = new QXmlStreamReader(&xmlFile);
 
-	QDir outDir = QDir("./out/");
 	if(!outDir.exists()) outDir.mkpath(".");
 
 	Converter converter = Converter(xml, &xmlFile, outDir);
