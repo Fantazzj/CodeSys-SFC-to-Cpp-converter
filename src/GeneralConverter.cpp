efbb5811920d6472b768a3e09199a0d4ac9f7145
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2023-02-16 11:57:42 +0100

    bugfix time with only minutes

--- src/GeneralConverter.cpp
+++ src/GeneralConverter.cpp
@@ -53,7 +53,10 @@ QStringList GeneralConverter::_convertTime(QString* time) {
 				qint64 val = value.sliced(nPos, i - nPos).toUInt();
 				if(value.at(i) == 'd') val = val * 24 * 60 * 60 * 1000;
 				if(value.at(i) == 'h') val = val * 60 * 60 * 1000;
-				if(value.at(i) == 'm' && value.at(i + 1) != 's') val = val * 60 * 1000;
+				if(value.at(i) == 'm') {
+					if(value.length()-1 <= i) val = val * 60 * 1000;
+					else if(value.at(i + 1) != 's') val = val * 60 * 1000;
+				}
 				if(value.at(i) == 's') val = val * 1000;
 				tot += val;
 				nPos = i + 1;
