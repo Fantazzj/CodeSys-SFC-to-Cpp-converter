f5a762c5fc7119cef14aecb44163b61c055e040d
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2023-07-24 19:24:44 +0200

    deployment made by cmake

--- deployWindows.bat
+++
@@ -1,6 +0,0 @@
-mkdir .\windows-deploy
-del .\windows-deploy\*
-move .\build-release\CodeSys-SFC-to-Cpp-Converter.exe .\windows-deploy
-windeployqt --no-translations .\windows-deploy\
-copy LICENSE .\windows-deploy
-copy README.md .\windows-deploy
